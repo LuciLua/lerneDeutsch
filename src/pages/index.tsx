56ef60e3c695e102879350727c33e1ecd3f5db1c
LuciLua <luci.lua81@gmail.com>
2021-06-06 13:26:12 -0300

    :cactus: Home 100%Width

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,64 +6,62 @@ type HomeProps = { }
 
 export default function Home( { }: HomeProps ) {
     return (   
-        <div>
+        <main>        
             <Head>
                 <title>Sonnezweits | Startseite</title>
             </Head>
-            <main>        
-                <div className={styles.baseMural}>
-                    <div className={styles.mural}>
-                        <div className={styles.title}>
-                            <h1>Hallo, du kannst jetzt deutsch lernen.<br/> Gemeinsam bewegen wir uns schneller</h1>
-                        </div>
-                        <div className={styles.card}>
-                            <h1>Hallo!</h1>
-                            <p>Machen Sie Ihre ersten Schritte</p>
-                            <button>anfangen
-                                <div className={styles.btnDet}>
-                                    <Image
-                                        width={0}
-                                        height={0} 
-                                        src={'/flag.svg'} 
-                                        alt={'profile'}
-                                        objectFit="cover"
-                                    />
-                                </div> 
-                            </button>
-                        </div>
-                        <div className={styles.card}>
-                            <h1>Wörter</h1>
-                            <p>lerne Gegenstände, Essen, Menschen zu sprechen ...</p>
-                            <button>anfangen
-                                <div className={styles.btnDet}>
-                                    <Image
-                                        width={0}
-                                        height={0} 
-                                        src={'/flag.svg'} 
-                                        alt={'profile'}
-                                        objectFit="cover"
-                                    />
-                                </div> 
-                            </button>
-                        </div>
-                        <div className={styles.card}>
-                            <h1>Audiovisuell</h1>
+            <div className={styles.baseMural}>
+                <div className={styles.mural}>
+                    <div className={styles.title}>
+                        <h1>Hallo, du kannst jetzt deutsch lernen.<br/> Gemeinsam bewegen wir uns schneller</h1>
+                    </div>
+                    <div className={styles.card}>
+                        <h1>Hallo!</h1>
+                        <p>Machen Sie Ihre ersten Schritte</p>
+                        <button>anfangen
+                            <div className={styles.btnDet}>
+                                <Image
+                                    width={0}
+                                    height={0} 
+                                    src={'/flag.svg'} 
+                                    alt={'profile'}
+                                    objectFit="cover"
+                                />
+                            </div> 
+                        </button>
+                    </div>
+                    <div className={styles.card}>
+                        <h1>Wörter</h1>
+                        <p>lerne Gegenstände, Essen, Menschen zu sprechen ...</p>
+                        <button>anfangen
+                            <div className={styles.btnDet}>
+                                <Image
+                                    width={0}
+                                    height={0} 
+                                    src={'/flag.svg'} 
+                                    alt={'profile'}
+                                    objectFit="cover"
+                                />
+                            </div> 
+                        </button>
+                    </div>
+                    <div className={styles.card}>
+                        <h1>Audiovisuell</h1>
                             <p>Finden Sie deutsche Kanäle, Podcasts und Videos</p>
-                            <button>anfangen
-                                <div className={styles.btnDet}>
-                                    <Image
-                                        width={0}
-                                        height={0} 
-                                        src={'/flag.svg'} 
-                                        alt={'profile'}
-                                        objectFit="cover"
-                                    />
-                                </div> 
-                            </button>
-                        </div>
+                        <button>anfangen
+                            <div className={styles.btnDet}>
+                                <Image
+                                    width={0}
+                                    height={0} 
+                                    src={'/flag.svg'} 
+                                    alt={'profile'}
+                                    objectFit="cover"
+                                />
+                            </div> 
+                        </button>
                     </div>
                 </div>
-            </main>
-        </div> 
+            </div>
+        </main>
     )
 }
