25fb0808969e848716776dc02819012ebb132b62
LuciLua <luci.lua81@gmail.com>
2021-06-10 20:21:06 -0300

    :cactus: 1sTransitionTime TextContentHalloAlign + wrap

--- src/pages/hallo.tsx
+++ src/pages/hallo.tsx
@@ -33,18 +33,20 @@ export default function Hallo ( {}: HalloProps){
                             <h3>familianname</h3>
                             <p>Último nome</p>
                         </div>
-                        <div className={styles.dialogoP}>
-                            <h2>Wie heißen Sie?</h2>
-                            <h3>Vi raissen zï?</h3>
-                            <p>Como se chama?</p>
-                        </div>
-                        <div className={styles.dialogoR}>
-                            <h2>Ich bin FRAU/HERR Motta</h2>
-                            <h3>Ish bin FRÄU/RÉR) + (FAMILIENNAME)</h3>
-                            <p>Eu sou Motta</p>
+                    </div>
+                        <div className={styles.dialogos}>
+                            <div className={styles.dialogoP}>
+                                <h2>Wie heißen Sie?</h2>
+                                <h3>Vi raissen zï?</h3>
+                                <p>Como se chama?</p>
+                            </div>
+                            <div className={styles.dialogoR}>
+                                <h2>Ich bin FRAU/HERR Motta</h2>
+                                <h3>Ish bin FRÄU/RÉR) + (FAMILIENNAME)</h3>
+                                <p>Eu sou Motta</p>
+                            </div>
                         </div>
 
-                    </div>
 
                 </div>
             </div>
